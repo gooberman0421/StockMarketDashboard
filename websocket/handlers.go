3cef86748831f890f0f55b1e944c57f19f853567
gooberman0421 <121976150+gooberman0421@users.noreply.github.com>
2024-09-17 16:38:12 +0300

    Develop handlers.go

---
+++ websocket/handlers.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/websocket"
+	"github.com/joho/godotenv"
+)
+
+var clients = make(map[*websocket.Conn]bool) 
+var broadcast = make(chan Message)           
+
+var upgrader = websocket.Upgrader{}
+
+type Message struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	fs := http.FileServer(http.Dir("./public"))
+	http.Handle("/", fs)
+
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleMessages()
+
+	log.Println("http server started on :" + os.Getenv("PORT"))
+	err = http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+
+	clients[ws] = true
+
+	for {
+		var msg Message
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(clients, ws)
+			break
+		}
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("write error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
